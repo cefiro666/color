2de8d98e3365aec5e91b520a8071ca3c8bd86c21
Vitaliy <45936078+cefiro666@users.noreply.github.com>
2022-11-30 22:26:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 mkdir -p ${HOME}/.bin
-cp color ${HOME}/.bin/color
+cp .build/release/color ${HOME}/.bin/color
 chmod 755 ${HOME}/.bin/color
 echo PATH="${HOME}/.bin:${PATH}" >> "${HOME}/.zshrc"
